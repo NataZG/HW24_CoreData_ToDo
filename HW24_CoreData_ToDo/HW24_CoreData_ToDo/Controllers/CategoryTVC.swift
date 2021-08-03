b275887f794c74121b637cb28c38cb626bd3bc60
NataZG <86071128+NataZG@users.noreply.github.com>
2021-08-03 19:20:04 +0300

    Category + Items set

--- HW24_CoreData_ToDo/HW24_CoreData_ToDo/Controllers/CategoryTVC.swift
+++ HW24_CoreData_ToDo/HW24_CoreData_ToDo/Controllers/CategoryTVC.swift
@@ -6,84 +6,116 @@
 //
 
 import UIKit
+import CoreData
 
-class CategoryVC: UITableViewController {
+class CategoryTVC: UITableViewController {
+    
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadCategories()
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return categories.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].name
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            // To delete from core data we need to fetch the object we are looking for
+            //
+            if let name = categories[indexPath.row].name {
+                let request: NSFetchRequest<Category> = Category.fetchRequest()
+                request.predicate = NSPredicate(format: "name MATCHES %@", name)
+                //request.predicate = NSPredicate(format: "name==\(category)")
+
+                if let categories = try? context.fetch(request) {
+                    for category in categories {
+                        context.delete(category)
+                    }
+                    // Save the context so our changes persist and We also have to delete the local copy of the data
+                    //
+                    self.categories.remove(at: indexPath.row)
+                    saveCategories()
+                    tableView.reloadData()
+                }
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: nil)
     }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let destinationVC = segue.destination as? ToDoListTVC {
+            if let indexPath = tableView.indexPathForSelectedRow {
+                destinationVC.selectedCategory = categories[indexPath.row]
+            }
+        }
     }
-    */
+    
+    
+    @IBAction func addBarButtonPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
 
+        alert.addTextField { textField in
+            textField.placeholder = "Category placeholder"
+        }
+
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
+        let action = UIAlertAction(title: "Add Category", style: .default) { _ in
+            if let textField = alert.textFields?.first {
+                if textField.text != "", let text = textField.text {
+                    let newCategory = Category(context: self.context)
+                    newCategory.name = text
+
+                    self.categories.append(newCategory)
+                    self.saveCategories()
+                    self.tableView.reloadData()
+                }
+            }
+        }
+
+        alert.addAction(action)
+        alert.addAction(cancel)
+
+        self.present(alert, animated: true)
+    }
+    
+    //MARK: - SAVE AND FETCH CATEGORIES FROM DB
+    
+    private func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context: \(error)")
+        }
+        tableView.reloadData()
+    }
+    
+    private func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error)")
+        }
+    }
 }
--- HW24_CoreData_ToDo/HW24_CoreData_ToDo/Controllers/ToDoListTVC.swift
+++ HW24_CoreData_ToDo/HW24_CoreData_ToDo/Controllers/ToDoListTVC.swift
@@ -6,40 +6,47 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListTVC: UITableViewController {
+    
+    var selectedCategory: Category? {
+        didSet {
+            self.title = selectedCategory?.name
+        }
+    }
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var itemArray = [Item]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        loadItems()
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return itemArray.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.accessoryType = itemArray[indexPath.row].done ? .checkmark : .none
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // deselect
+        tableView.deselectRow(at: indexPath, animated: true)
+//        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        itemArray[indexPath.row].done.toggle()
+        self.saveItems()
+        tableView.reloadRows(at: [indexPath], with: .fade)
+    }
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -57,7 +64,7 @@ class ToDoListTVC: UITableViewController {
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     */
 
@@ -86,4 +93,80 @@ class ToDoListTVC: UITableViewController {
     }
     */
 
+    
+    @IBAction func addItemPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Add New Item", message: "", preferredStyle: .alert)
+
+        alert.addTextField { textField in
+            textField.placeholder = "Your task"
+        }
+
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { _ in }
+        let action = UIAlertAction(title: "Add Item", style: .default) { _ in
+            if let textField = alert.textFields?.first {
+                if textField.text != "", let title = textField.text {
+                    let newItem = Item(context: self.context)
+                    newItem.title = title
+                    newItem.done = false
+                    newItem.parentCategory = self.selectedCategory
+
+                    self.itemArray.append(newItem)
+                    self.tableView.reloadData()
+                    self.saveItems()
+                }
+            }
+        }
+
+        alert.addAction(action)
+        alert.addAction(cancel)
+
+        self.present(alert, animated: true)
+    }
+    
+    //MARK: - SAVE AND FETCH ITEMS FROM DB
+    
+    private func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+        if let name = selectedCategory?.name {
+            let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", name)
+
+            request.predicate = categoryPredicate
+
+            do {
+                itemArray = try context.fetch(request)
+            } catch {
+                print("Error fetching data from context: \(error)")
+            }
+            tableView.reloadData()
+        }
+    }
+    
+    
+    // Универсальная ф-я со входным предикатом
+    
+//    private func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+//        if let name = selectedCategory?.name {
+//            let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", name)
+//
+//            if let additionalPredicate = predicate {
+//                request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+//            } else {
+//                request.predicate = categoryPredicate
+//            }
+//
+//            do {
+//                itemArray = try context.fetch(request)
+//            } catch {
+//                print("Error fetching data from context: \(error)")
+//            }
+//            tableView.reloadData()
+//        }
+//    }
+    
+    private func saveItems() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error)")
+        }
+    }
 }
